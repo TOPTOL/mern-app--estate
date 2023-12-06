757a2d87548dc44de6b432783fc0db209156eaa8
seye <ilesanmiseye@yahoo.com>
2023-12-06 18:51:34 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <h1 className='text-red-500'>Real Estate App using MERN Stack and Tailwind CSS</h1>
   )
 }
