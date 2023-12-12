338f031a0091dab9fd1c60e918f20314c8dc26d3
seye <ilesanmiseye@yahoo.com>
2023-12-12 23:14:08 +0100

    install react js and tailwind css and create the first template

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+
 
 export default function App() {
   return <BrowserRouter>
