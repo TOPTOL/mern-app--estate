a7bb0e06400c4970decb0c42ca573638eeba9e36
seye <ilesanmiseye@yahoo.com>
2023-12-15 00:13:53 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './Components/Header';
+
+
+
 
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
 
-  </Routes> 
-  </BrowserRouter>
+      </Routes> 
+    </BrowserRouter>
+  );
 }
+
---
+++ src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
+            <Link to='/'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>TOPTOL</span>
+                <span className='text-slate-700'><i>GARDENS</i></span>
+            
+            </h1>
+            </ Link>
+
+            <form className='bg-slate-100 p-3 rounded-lg  flex item-center'>
+                <input type='text' 
+                placeholder='search...'
+                 className='bg-transparent focus:outline-none w-24 sm:w-64'
+                 />
+                <FaSearch className='text-slate-600' />
+            </form>
+            <ul className='flex gap-4 marginbottom-60'>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'> Home</li>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'> About</li>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'> Sign in</li>
+
+            </ul>
+        </div>
+    </header>
+  )
+}
