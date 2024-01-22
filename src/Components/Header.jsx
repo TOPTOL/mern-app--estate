83bb69ca2009fb382fde559de87107ffacc34238
seye <ilesanmiseye@yahoo.com>
2024-01-22 11:31:13 +0100

    sign up fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -33,9 +33,9 @@ export default function Header() {
                 About</li>
                 </Link>
 
-                <Link to='/sign in'>
+                <Link to='/sign-in'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'> 
-                Sign in</li>
+                Sign In</li>
 
                 </Link>
 
